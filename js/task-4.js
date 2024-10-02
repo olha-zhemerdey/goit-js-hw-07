8699cb83839c74927d894640c211937511e5a08b
Olha <oyashnaya88@gmail.com>
2024-10-02 11:46:41 +0300

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,7 +1,16 @@
 const form = document.querySelector(".login-form");
 
-form.addEventListener('submit', handlerSubmit);
+form.addEventListener("submit", handlerSubmit);
 
-form.addEventListener("submit", event => {
-	
-});
+function handlerSubmit(event) {
+  event.preventDefault();
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+  if (email === "" || password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();
+}
